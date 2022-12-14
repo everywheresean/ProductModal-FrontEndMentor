d10cfb9ee6f96f847790ad1c5520054aec32ddfd
everywheresean <sean.n.do@gmail.com>
2022-12-14 15:48:44 -0600

    basic layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      'sans': ['Montserrat', 'sans'],
+      'serif': ['Fraunces', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        veryDarkBlue: 'hsl(212, 21%, 14%)',
+        darkGrayishBlue: 'hsl(228,12%, 48%)',
+        cream: 'hsl(30, 38%, 92%)',
+        darkCyan: 'hsl(158, 36%, 37%)',
+      },
+    },
   },
   plugins: [],
 }
